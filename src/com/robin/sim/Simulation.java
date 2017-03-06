e869245047c9acd1e27fd452c63105c84a27282c
RobinTPotter <r@p.com>
2017-03-06 17:43:01 +0000

    worm

--- src/com/robin/sim/Simulation.java
+++ src/com/robin/sim/Simulation.java
@@ -11,54 +11,103 @@ import android.view.View;
  */
 public class Simulation implements View.OnTouchListener, GestureDetector.OnGestureListener, ScaleGestureDetector.OnScaleGestureListener {
 
+    private interface WormWrangler {
+        public void setNewTarget(Worm w);
+    }
+
+    private class Worm {
+        double x, y, size;
+        double targetx;
+        double targety;
+        double dirx;
+        double diry;
+        double mag;
+
+        double MAX_ANGLE = (60.0 * 2 * Math.PI / 360);
+        double state = 0;
+
+        double speed = 0.5;
+
+        Paint p = new Paint();
+        Simulation wormWrangler;
+
+        public Worm(Simulation ww) {
+            this.wormWrangler = ww;
+        }
+
+        public void update() {
+
+            dirx = targetx - x;
+            diry = targety - y;
+            mag = Math.sqrt(dirx * dirx + diry * diry);
+
+            if (mag < 10) wormWrangler.setNewTarget(this);
+
+            dirx *= size / mag;
+            diry *= size / mag;
+
+            x += dirx * speed;
+            y += diry * speed;
+
+        }
+
+        public void draw(Canvas c) {
+
+            c.drawLine((float) (x + 0 * dirx), (float) (y + 0 * diry), (float) (x + 1 * dirx), (float) (y + 1 * diry), p);
+
+        }
+    }
+
     SimView simView;
+    Worm[] worms;
+    int width, height;
 
     public Simulation(SimView simView) {
         this.simView = simView;
     }
 
+    public void setNewTarget(Worm w) {
+
+        w.targetx = Math.random() * width;
+        w.targety = Math.random() * height;
+
+    }
+
     protected void updateProperties(int width, int height) {
+        this.width = width;
+        this.height = height;
+        if (worms == null) {
+
+            worms = new Worm[10];
+            for (Worm w : worms) {
+                w = new Worm(this);
+                w.size = (int) (10 * Math.random()) + 5;
+                w.x = (Math.random() * width);
+                w.y = (Math.random() * height);
+                w.targetx = (Math.random() * width);
+                w.targety = (Math.random() * height);
+                w.state = Math.random();
+            }
+
+        }
+
+        for (Worm w : worms) {
+            w.update();
+        }
+        
     }
 
     protected void drawMethod(int width, int height) {
 
         //defaultDrawMethod(width, height);
 
-    }
-
-    protected void defaultDrawMethod(int width, int height) {
-
-        Canvas canvas = new Canvas(simView.getBuffer());
-
-        Paint p = new Paint();
-        int rr = 0, gg = 0, bb = 0;
-        float ll = 0;
-
-        while (ll < 0.5) {
-
-            rr = (int) (Math.random() * 255);
-            gg = (int) (Math.random() * 255);
-            bb = (int) (Math.random() * 255);
-
-            ll = (0.2126f * rr + 0.7152f * gg + 0.0722f * bb);
+        for (Worm w : worms) {
+            w.draw(new Canvas(simView.getBuffer()));
         }
-
-        p.setARGB(255, rr, gg, bb);
-
-        canvas.drawRect(new Rect(
-                        (int) (Math.random() * width), (int) (Math.random() * height),
-                        (int) (Math.random() * width), (int) (Math.random() * height)),
-                p);
-
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-
-        Canvas c = new Canvas(simView.getBuffer());
-        Paint p = new Paint();
-        p.setColor(Color.RED);
-        c.drawOval(new RectF(event.getX() - 10, event.getY() - 10, event.getX() + 10, event.getY() + 10), p);
 
         //simView.message("touch");
         boolean ret = simView.getGestureDetector().onTouchEvent(event);
@@ -92,21 +141,13 @@ public class Simulation implements View.OnTouchListener, GestureDetector.OnGestu
     @Override
     public void onLongPress(MotionEvent e) {
         //simView.message("long press");
-        Canvas c = new Canvas(simView.getBuffer());
-        Paint p = new Paint();
-        p.setColor(Color.CYAN);
-        p.setStyle(Paint.Style.STROKE);
-        c.drawOval(new RectF(e.getX()-20,e.getY()-20,e.getX()+20,e.getY()+20), p);
+
     }
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         // simView.message("fling");
-        Canvas c = new Canvas(simView.getBuffer());
-        Paint p = new Paint();
-        p.setColor(Color.CYAN);
-        c.drawLine(e1.getX(),e1.getY(),e2.getX(),e2.getY(), p);
-        return false;
+        return true;
     }
 
     @Override
@@ -114,16 +155,6 @@ public class Simulation implements View.OnTouchListener, GestureDetector.OnGestu
         //simView.message("scale");
         //
 
-        Canvas c = new Canvas(simView.getBuffer());
-        Paint p = new Paint();
-        p.setColor(Color.YELLOW);
-        p.setStyle(Paint.Style.STROKE);
-        c.drawRect(new RectF(
-                detector.getFocusX() - detector.getCurrentSpanX() / 2,
-                detector.getFocusY() - detector.getCurrentSpanY() / 2,
-                detector.getFocusX() + detector.getCurrentSpanX() / 2,
-                detector.getFocusY() + detector.getCurrentSpanY() / 2
-        ), p);
         return true;
     }
 
@@ -131,30 +162,12 @@ public class Simulation implements View.OnTouchListener, GestureDetector.OnGestu
     public boolean onScaleBegin(ScaleGestureDetector detector) {
         //simView.message("scale begin");
 
-        Canvas c = new Canvas(simView.getBuffer());
-        Paint p = new Paint();
-        p.setColor(Color.GREEN);
-        c.drawRect(new RectF(
-                detector.getFocusX() - detector.getCurrentSpanX() / 2,
-                detector.getFocusY() - detector.getCurrentSpanY() / 2,
-                detector.getFocusX() + detector.getCurrentSpanX() / 2,
-                detector.getFocusY() + detector.getCurrentSpanY() / 2
-        ), p);
-
         return true;
     }
 
     @Override
     public void onScaleEnd(ScaleGestureDetector detector) {
         //simView.message("scale end");
-        Canvas c = new Canvas(simView.getBuffer());
-        Paint p = new Paint();
-        p.setColor(Color.BLUE);
-        c.drawRect(new RectF(
-                detector.getFocusX() - detector.getCurrentSpanX() / 2,
-                detector.getFocusY() - detector.getCurrentSpanY() / 2,
-                detector.getFocusX() + detector.getCurrentSpanX() / 2,
-                detector.getFocusY() + detector.getCurrentSpanY() / 2
-        ), p);
+
     }
 }
