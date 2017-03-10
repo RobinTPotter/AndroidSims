73b4901bf717362d582dd05c9be39aba7501f279
deadmeat <robin.t.potter@googlemail.com>
2017-03-10 22:12:44 +0000

    store etc

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/deadmeat/test/AndroidSims/bin/AndroidManifest.xml : \
+/home/deadmeat/test/AndroidSims/AndroidManifest.xml \
---
+++ bin/classes.dex.d
@@ -0,0 +1,16 @@
+/home/deadmeat/test/AndroidSims/bin/classes.dex : \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/R.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/SimView.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/R$drawable.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/SimThread.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/Simulation.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/BuildConfig.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/Sim.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/Manifest.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/R$attr.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/R$string.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/Worm$WormWrangler.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/Worm.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/R$id.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/SimView$1.class \
+/home/deadmeat/test/AndroidSims/bin/classes/com/robin/sim/R$layout.class \
